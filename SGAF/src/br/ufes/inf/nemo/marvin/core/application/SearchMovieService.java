f7a96165b147f4518c7b3330486a3024266cca3f
Rodolfocostapr <rodolfocostapr@gmail.com>
2017-10-17 22:43:58 -0200

    add do service para pesquisa de filmes.

---
+++ SGAF/src/br/ufes/inf/nemo/marvin/core/application/SearchMovieService.java
@@ -0,0 +1,10 @@
+package br.ufes.inf.nemo.marvin.core.application;
+
+import java.io.Serializable;
+
+import javax.ejb.Local;
+
+@Local
+public interface SearchMovieService extends Serializable {
+
+}
