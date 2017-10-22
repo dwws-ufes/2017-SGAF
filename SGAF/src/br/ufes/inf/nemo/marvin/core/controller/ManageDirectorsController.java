1ff284d839d9ff8f44c0adde752f2f121d59b009
JefersonBatista <jeferson.de.oliveira.batista@gmail.com>
2017-10-22 17:20:32 -0200

    CRUD de diretores.

--- SGAF/src/br/ufes/inf/nemo/marvin/core/controller/ManageDirectorsController.java
+++ SGAF/src/br/ufes/inf/nemo/marvin/core/controller/ManageDirectorsController.java
@@ -7,31 +7,29 @@ import javax.inject.Named;
 import br.ufes.inf.nemo.jbutler.ejb.application.CrudService;
 import br.ufes.inf.nemo.jbutler.ejb.application.filters.SimpleFilter;
 import br.ufes.inf.nemo.jbutler.ejb.controller.CrudController;
-import br.ufes.inf.nemo.marvin.core.application.ManageGenresService;
-import br.ufes.inf.nemo.marvin.core.domain.Genre;
+import br.ufes.inf.nemo.marvin.core.application.ManageDirectorsService;
+import br.ufes.inf.nemo.marvin.core.domain.Director;
 
 @Named
 @SessionScoped
-public class ManageGenresController extends CrudController<Genre> {
-
+public class ManageDirectorsController extends CrudController<Director> {
 	/** The serialization id. */
 	private static final long serialVersionUID = 1L;
 
-	/** The "Manage Genres" service. */
+	/** The "Manage Directors" service. */
 	@EJB
-	private ManageGenresService manageGenresService;
+	private ManageDirectorsService manageDirectorsService;
 
 	/** @see br.ufes.inf.nemo.jbutler.ejb.controller.CrudController#getCrudService() */
 	@Override
-	protected CrudService<Genre> getCrudService() {
-		return manageGenresService;
+	protected CrudService<Director> getCrudService() {
+		return manageDirectorsService;
 	}
 
 	/** @see br.ufes.inf.nemo.jbutler.ejb.controller.ListingController#initFilters() */
 	@Override
 	protected void initFilters() {
-		addFilter(new SimpleFilter("manageGenres.filter.byName", "name",
-				getI18nMessage("msgsCore", "manageGenres.text.filter.byName")));
+		addFilter(new SimpleFilter("manageDirectors.filter.byName", "name",
+				getI18nMessage("msgsCore", "manageDirectors.text.filter.byName")));
 	}
-
 }
