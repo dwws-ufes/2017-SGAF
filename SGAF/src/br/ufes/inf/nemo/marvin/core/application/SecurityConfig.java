9f3a8ffa818310f16b3e16db6310e5e4e9d112de
2017133150 <2017133150@Grad07>
2017-10-23 10:59:34 -0200

    tentando botar o custom form

--- SGAF/src/br/ufes/inf/nemo/marvin/core/application/SecurityConfig.java
+++ SGAF/src/br/ufes/inf/nemo/marvin/core/application/SecurityConfig.java
@@ -2,14 +2,31 @@ package br.ufes.inf.nemo.marvin.core.application;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Autowired
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("rodo").password("123").roles("USER");
+		auth.inMemoryAuthentication().withUser("ro").password("123").roles("USER");
+	}
+	
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+        http
+        .authorizeRequests()//	
+            .anyRequest().authenticated()// 
+            .and()//	
+        .formLogin()//                      
+        .loginPage("/core/login/index.faces")//
+        .permitAll()//
+        .usernameParameter("email")//
+        .passwordParameter("password")//
+        .and().logout().permitAll();
 	}
 
 }
